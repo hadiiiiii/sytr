44c0a4ce3bf9ff4662d806f5296b66cd4a979d21
hadiiiiii <45215673+hadiiiiii@users.noreply.github.com>
2018-12-14 02:36:10 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2863,6 +2863,15 @@ client.on('message',async message => {
 
 
 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown:اسم العضو  ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+
 
 
 
